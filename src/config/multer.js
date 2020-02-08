b6f45b474a91cf57017f0e03287e9d0bc19756b4
Natanael Lima <taelima1997@gmail.com>
2020-02-08 01:05:33 -0300

    finish apllication

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,14 +1,32 @@
+require('dotenv/config')
 const multer = require('multer')
 const path = require('path')
 const crypto = require('crypto')
+const multerS3 = require('multer-s3')
+const aws = require('aws-sdk')
 
-module.exports = {
-  dest: path.resolve(__dirname, '..', '..', 'tmp'),
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, '..', '..', 'tmp'))
     },
     filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, has) => {
+        if (err) cb(err)
+
+        file.key = `${has.toString('hex')}-${file.originalname}`
+
+        cb(null, file.key)
+      })
+    },
+  }),
+
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: 'uploadnatanaelvich',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
       crypto.randomBytes(16, (err, has) => {
         if (err) cb(err)
 
@@ -18,6 +36,11 @@ module.exports = {
       })
     },
   }),
+}
+
+module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'tmp'),
+  storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024,
   },
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const path = require('path')
 require('dotenv/config')
 require('./service/mongoose')
 
@@ -9,6 +10,7 @@ const server = express()
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 server.use(morgan('dev'))
+server.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')))
 server.use(routes)
 
 server.listen(3333, () => {
